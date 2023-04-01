c89e45aa6f22366c3e57f9e94cbbf877c1336f75
gokuhieu <103849197+bunpro1910@users.noreply.github.com>
2023-04-01 10:33:47 +0700

    v1

--- server/server.js
+++ server/server.js
@@ -1,80 +1,56 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-const url  =require('url');
-const cors =require("cors")
+const url = require('url');
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 
-const Main_Routes = require('./Routes/Main')
-const Admin_Routes = require('./Routes/Admin')
-const User_Routes = require('./Routes/User')
-const {isuser,isadmin} =require('./Models/check')
+const Main_Routes = require('./Routes/Main');
+const Admin_Routes = require('./Routes/Admin');
+const User_Routes = require('./Routes/User');
+const { isuser, isadmin } = require('./Models/check');
 
-var cookieSession = require('cookie-session')
-const morgan =require('morgan')
-const bodyParser = require('body-parser')
-app.set('trust proxy', 1)
+var cookieSession = require('cookie-session');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.set('trust proxy', 1);
 app.set('view engine', 'html');
 
-const {Server} = require('socket.io')
-// app.use(cors())
+const { Server } = require('socket.io');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true  }));
-app.use(express.urlencoded({ extended: true })); 
-app.use('/public',express.static((__dirname+ '/public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static((__dirname + '/public')));
 
 app.use(cookieSession({
-    name: 'session',
-    keys: ["phamlehaison"],
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    httpOnly:false
-  }))
-
-
+  name: 'session',
+  keys: ["phamlehaison"],
+  maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  httpOnly: false
+}));
 
 app.use(morgan('dev'));
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://client-shop.onrender.com');
- 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  res.setHeader('Access-Control-Allow-Origin', 'https://client-shop.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
+app.use("/api/", Main_Routes);
 
+app.use("/api/admin", isadmin, Admin_Routes);
+app.use("/api/user", isuser, User_Routes);
 
-
-
-app.use("/api/",Main_Routes)
-
-app.use("/api/admin",isadmin,Admin_Routes)
-app.use("/api/user",isuser,User_Routes)
-var server = require('https').Server(app)
-global.io = new Server(server,{cors: {
-    origin: 'https://client-shop.onrender.com',
-}});
-
-
+var server = require('https').Server(app);
+global.io = new Server(server);
 
 io.on('connection', (socket) => {
-  
+
 });
-
-
 
 server.listen(port, () => {
-    console.log(`Application started and Listening on port ${port}`);
+  console.log(`Application started and Listening on port ${port}`);
 });
-
-          
